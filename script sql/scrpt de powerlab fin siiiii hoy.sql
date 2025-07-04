4f1b6dfe232a01094b5f6a08dc228b853908b348
mildred123456 <callejasmildred7@gmail.com>
2025-07-04 13:46:41 -0500

    adjunto script de sql

---
+++ script sql/scrpt de powerlab fin siiiii hoy.sql
@@ -0,0 +1,71 @@
+use powerlab;
+DROP TABLE IF EXISTS j;
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    fecha_nacimiento DATE NOT NULL,
+    genero VARCHAR(10) NOT NULL,
+    rol VARCHAR(50) NOT NULL,
+    estado CHAR(1) DEFAULT 'A' ,-- 'A' = Activo, 'I' = Inactivo (eliminado)
+    contrasenia VARCHAR(255) NOT NULL
+
+);
+
+
+CREATE TABLE rutinas (
+    nombre VARCHAR(100) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    descripcion TEXT,
+    nivel ENUM('principiante', 'intermedio', 'avanzado') NOT NULL,
+    dias_por_semana INT NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_instructor INT,
+    FOREIGN KEY (id_instructor) REFERENCES usuario(id)
+);
+
+INSERT INTO usuario (
+    nombre,
+    apellido,
+    correo,
+    fecha_nacimiento,
+    genero,
+    rol,
+    estado,
+    contrasenia
+) VALUES (
+    'mildred',
+    'callejas',
+    'admin@powerlab.com',
+    '1990-01-01',
+    'femenino',
+    'administrador',
+    'A',
+    'admin123'
+);
+
+INSERT INTO usuario (
+    nombre,
+    apellido,
+    correo,
+    fecha_nacimiento,
+    genero,
+    rol,
+    estado,
+    contrasenia
+) VALUES (
+    'pepa',
+    'castalleda',
+    'pepa@powerlab.com',
+    '1990-01-01',
+    'femenino',
+    'deportista',
+    'A',
+    'daniela123'
+);
+
+SELECT * FROM rutinas;
+SELECT * FROM usuario;
